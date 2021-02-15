dd60204aaea28f2f92b8293bf2754e54ae9aab10
github username <github email address>
2021-02-15 17:47:12 -0500

    almost have the queens from the season

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 // Query Selector //
 countryPage = document.querySelector("#country")
 seasonAllQueens = document.querySelector(".queenContentwrapper")
-countryId = 1
-seasonId = 1
+let countryId = 1
 
 // Event Listener //
 countryPage.addEventListener("click", e => {selectCountry(e.target.dataset.id)})
@@ -18,33 +17,41 @@ function showCountries () {
 function selectCountry (countryId) {
     fetch(`http://localhost:3000/countries/${countryId}`) 
     .then(response => response.json())
-    .then(data => compareCountryId(data))
+    .then(data => accessSelectedCountryQueens(data))
     
 }
 
 function showSeasonQueens() {
     fetch(`http://localhost:3000/queens/`) 
     .then(response => response.json())
-    .then(data => seasonQueens)
+    .then(data => data.forEach(element => seasonQueens(element)))
 }
 // Function //
 
-function compareCountryId(e) {
-    return e.id
+function accessSelectedCountryQueens(e) {
+    countryId = e.id
+    fetch(`http://localhost:3000/queens/`) 
+    .then(response => response.json())
+    .then(function(response) {(element => if (element.country_id === countryId ? console.log(e.image) : console.log(e.name)))
 }
+.then(data => data.forEach(element)
+// .then(function(response) {
+//     if (element.country_id === countryId {
+//        // do something
+//      };
+//    })
 function showAllCountries(e) {
     let span = document.createElement("img")
     span.src = e.image
     span.dataset.id = e.id
-    countryId = 1
     countryPage.append(span)
 }
 
-function seasonQueens(e) {
-    console.log(e)
-//  e.country_id === compareCountryId(e.id) ? console.log(e.image) : console.log(e.name)
-}
+// function seasonQueens(e) {
+// //  e.country_id === countryId ? console.log(e.image) : console.log(e.name)
+// }
 
 
 
 showCountries()
+showSeasonQueens()
