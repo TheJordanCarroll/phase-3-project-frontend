a6e0b8d3f78cb4357352cd09f53a05ffd59336aa
github username <github email address>
2021-02-15 15:37:20 -0500

    cant display countries

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// Query Selector //
+countryPage = document.querySelector("#country")
+console.log(countryPage)
+// Event Listener //
+
+
+// Fetch //
+
+function showCountries () {
+    fetch("http://localhost:3000/countries") 
+    .then(response => response.json())
+    .then(data => data.forEach(element => showAllCountries(element)))
+}
+
+// Function //
+
+function showAllCountries(e) {
+    let span = document.createElement("img")
+    span.src = e.image
+    span.dataset.id = e.id
+    countryPage.append(span)
+}
+
+showCountries()
