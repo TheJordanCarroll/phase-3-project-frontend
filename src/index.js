435a71b044e08d9e7f179e3b2318deb43f2ff662
github username <github email address>
2021-02-15 18:02:19 -0500

    gif

--- src/index.js
+++ src/index.js
@@ -32,14 +32,15 @@ function accessSelectedCountryQueens(e) {
     countryId = e.id
     fetch(`http://localhost:3000/queens/`) 
     .then(response => response.json())
-    .then(function(response) {(element => if (element.country_id === countryId ? console.log(e.image) : console.log(e.name)))
+    .then(data => data.forEach(element => element.country_id === countryId ? showAllQueens(element) : ""))
 }
-.then(data => data.forEach(element)
-// .then(function(response) {
-//     if (element.country_id === countryId {
-//        // do something
-//      };
-//    })
+
+function showAllQueens(e) {
+    let span = document.createElement("img")
+    span.src = e.gif
+    span.dataset.id = e.id
+    countryPage.append(span)
+}
 function showAllCountries(e) {
     let span = document.createElement("img")
     span.src = e.image
