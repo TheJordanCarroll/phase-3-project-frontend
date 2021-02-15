affab7a0e0c1ddaf853848605f12ee10d6da20e3
github username <github email address>
2021-02-15 16:19:54 -0500

    working on showing queens

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 // Query Selector //
 countryPage = document.querySelector("#country")
-console.log(countryPage)
-// Event Listener //
+seasonAllQueens = document.querySelector(".queenContentwrapper")
+countryId = 1
 
+// Event Listener //
+countryPage.addEventListener("click", e => {selectCountry(e.target.dataset.id)})
 
 // Fetch //
 
@@ -12,13 +14,30 @@ function showCountries () {
     .then(data => data.forEach(element => showAllCountries(element)))
 }
 
+function selectCountry (countryId) {
+    fetch(`http://localhost:3000/countries/${countryId}`) 
+    .then(response => response.json())
+    .then(data => showSeasonQueens(data))
+}
+
+function showSeasonQueens() {
+    fetch(`http://localhost:3000/queens/`) 
+    .then(response => response.json())
+    .then(data => data.forEach(element => seasonQueens(element)))
+}
 // Function //
 
 function showAllCountries(e) {
     let span = document.createElement("img")
     span.src = e.image
     span.dataset.id = e.id
+    countryId = 1
     countryPage.append(span)
+}
+
+function seasonQueens(e) {
+    countryId = e.country_id
+    console.log(countryId)
 }
 
 showCountries()
