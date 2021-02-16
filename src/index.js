1a1735961837fe4583a59832f6c87d217c2a71ab
Jordan Carroll <jordancarroll1994@gmail.com>
2021-02-16 17:23:10 -0500

    cleaned the HTML

--- src/index.js
+++ src/index.js
@@ -8,21 +8,22 @@ let countryId = 1
 // Event Handling //
 countriesPage.addEventListener("click", e => {selectCountry(e.target.dataset.id)})
 // queensPage.addEventListener("click", e => {getSingleQueen(e.target.dataset.id)})
-queensPage.addEventListener("click", handleClick)
+// queensPage.addEventListener("click", handleClick)
 
-function handleClick(e){
+function queenGifClick(e){
+    // console.log(e.target.class)
     if (e.target.class === "rendered-queen"){
-      const queenId = e.target.dataset.id
-      getSingleQueen(queenId)
+        const queenId = e.target.dataset.id
+        getSingleQueen(queenId)
     }
 }
 
 // Fetch/Network Requests //
-// function showTeams() {
-//     fetch("http://localhost:3000/teams") 
-//     .then(response => response.json())
-//     .then(data => data.forEach(element => showAllCountries(element)))
-// }
+function showTeams() {
+    fetch("http://localhost:3000/teams") 
+    .then(response => response.json())
+    .then(data => data.forEach(element => showAllTeams(element)))
+}
 
 function showCountries() {
     fetch("http://localhost:3000/countries") 
@@ -45,17 +46,20 @@ function accessSelectedCountryQueens(e) {
 function getSingleQueen(queenId){
     fetch(`http://localhost:3000/queens/${queenId}`)
     .then(res => res.json())
-    .then(data => addQueenShowSection)
+    .then(data => addQueenShowSection(data))
 }
 
 // Manipulating the DOM //
 function showAllQueens(e) {
     let span = document.createElement("img")
+    let gifDiv = document.createElement("div")
     span.class = "rendered-queen"
     span.src = e.gif
     span.dataset.id = e.id
+    gifDiv.addEventListener('click', queenGifClick)
     countriesPage.remove()
-    queensPage.append(span)
+    gifDiv.append(span)
+    queensPage.append(gifDiv)
 }
 
 function showAllCountries(e) {
@@ -68,13 +72,24 @@ function showAllCountries(e) {
 function addQueenShowSection(singleQueen) {
     queenContainer.innerHTML = `
     <h2 class="queen-name">Name: ${singleQueen.name}</h2>
-    <img class="queen-image" src=${singleQueen.gif} alt=${singleQueen.name} />
-    <h3 class="country-name">Country: ${singleQueen.country.name}</h3>
+    <img class="queen-image" src=${singleQueen.grid_image} alt=${singleQueen.name} />
+    <h3 class="country-name">Show: ${singleQueen.country.name}</h3>
     <h3 class="season-name">Season: ${singleQueen.season}</h3>
     <a class="queen-instagram" href="${singleQueen.instagram}">Instagram</a>
     <a class="queen-twitter" href="${singleQueen.twitter}">Twitter</a>
     `
 }
 
+function showAllTeams(e) {
+    let teamDiv = document.createElement("div")
+    teamDiv.innerHTML = `
+    <h2 class="team-name">Name: ${e.name}</h2>
+    <img class="team-image" src=${e.image} alt=${e.name} />
+    <h3 class="team-country">Show: ${e.country.name}</h3>
+    `
+}
+
 showCountries()
+
+// <h3 class="country-name">Country: ${singleQueen.country.name}</h3>
 
