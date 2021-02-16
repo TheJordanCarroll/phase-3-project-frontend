90844667df14bad1193b9d4aadb8943bbdc2c9a9
Jordan Carroll <jordancarroll1994@gmail.com>
2021-02-16 13:52:50 -0500

    tuesday afternoon

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-// Query Selectors //
-countriesPage = document.querySelector("#countries")
-queensPage = document.querySelector(".queens")
+// Grab DOM Elements //
+const countriesPage = document.querySelector(".countries")
+const queensPage = document.querySelector(".queens")
+const queenContainer = document.querySelector(".queen-container")
+const teamsPage = document.querySelector(".teams")
 let countryId = 1
 
-// Event Listeners //
-countryPage.addEventListener("click", e => {selectCountry(e.target.dataset.id)})
-queensPage.addEventListener("click", handleQueenClick)
+// Event Handling //
+countriesPage.addEventListener("click", e => {selectCountry(e.target.dataset.id)})
+// queensPage.addEventListener("click", e => {getSingleQueen(e.target.dataset.id)})
+queensPage.addEventListener("click", handleClick)
 
-// Fetch Requests //
+function handleClick(e){
+    if (e.target.class === "rendered-queen"){
+      const queenId = e.target.dataset.id
+      getSingleQueen(queenId)
+    }
+}
+
+// Fetch/Network Requests //
+// function showTeams() {
+//     fetch("http://localhost:3000/teams") 
+//     .then(response => response.json())
+//     .then(data => data.forEach(element => showAllCountries(element)))
+// }
+
 function showCountries() {
     fetch("http://localhost:3000/countries") 
     .then(response => response.json())
@@ -18,42 +34,46 @@ function selectCountry(countryId) {
     fetch(`http://localhost:3000/countries/${countryId}`) 
     .then(response => response.json())
     .then(data => accessSelectedCountryQueens(data))
-    
 }
 
-function showSeasonQueens() {
-    fetch(`http://localhost:3000/queens/`) 
-    .then(response => response.json())
-    .then(data => data.forEach(element => seasonQueens(element)))
-}
-
-// Functions //
 function accessSelectedCountryQueens(e) {
     fetch(`http://localhost:3000/queens/`) 
     .then(response => response.json())
     .then(data => data.forEach(element => element.country_id === e.id ? showAllQueens(element) : ""))
 }
 
+function getSingleQueen(queenId){
+    fetch(`http://localhost:3000/queens/${queenId}`)
+    .then(res => res.json())
+    .then(data => addQueenShowSection)
+}
+
+// Manipulating the DOM //
 function showAllQueens(e) {
     let span = document.createElement("img")
     span.class = "rendered-queen"
     span.src = e.gif
     span.dataset.id = e.id
-    seasonAllQueens.append(span)
+    countriesPage.remove()
+    queensPage.append(span)
 }
 
 function showAllCountries(e) {
     let span = document.createElement("img")
     span.src = e.image
     span.dataset.id = e.id
-    countryPage.append(span)
+    countriesPage.append(span)
 }
 
-function handleQueenClick(e){
-    if (e.target.className === "rendered-queen"){
-      const queenId = e.target.dataset.id
-      getSingleQueen(queenId)
-    }
+function addQueenShowSection(singleQueen) {
+    queenContainer.innerHTML = `
+    <h2 class="queen-name">Name: ${singleQueen.name}</h2>
+    <img class="queen-image" src=${singleQueen.gif} alt=${singleQueen.name} />
+    <h3 class="country-name">Country: ${singleQueen.country.name}</h3>
+    <h3 class="season-name">Season: ${singleQueen.season}</h3>
+    <a class="queen-instagram" href="${singleQueen.instagram}">Instagram</a>
+    <a class="queen-twitter" href="${singleQueen.twitter}">Twitter</a>
+    `
 }
 
 showCountries()
